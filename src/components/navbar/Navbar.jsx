b31feab92f4c58e17eed184108925e293c21e79d
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-02 11:57:02 +0300

    add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,15 @@ import Logo from '../../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <div className={style.navbar}>
-      <div>
-        <img src={Logo} alt='/' />
-      </div>
-      <div className={style.btn}>
-        <button>Log In</button>
-        <button>Sign Up</button>
+    <div className={style.line}>
+      <div className={style.navbar}>
+        <div>
+          <img src={Logo} alt='/' />
+        </div>
+        <div className={style.btn}>
+          <button>Log In</button>
+          <button>Sign Up</button>
+        </div>
       </div>
     </div>
   );
