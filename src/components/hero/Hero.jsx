878e7c8f5ac6b9472972ac23c015c592c10051bf
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-02 18:01:06 +0300

    add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,18 +5,20 @@ import Background from '../../assets/background.png';
 const Hero = () => {
   return (
     <div className={style.hero}>
-      <div>
-        <img src={Background} alt='/' />
-      </div>
+      <img src={Background} alt='/' />
 
       <div className={style.block}>
-        <div className={style.text}>
+        <div className={style.header}>
           <h2>TRENDING PRODUCT</h2>
           <h1>Business Network for Logistics</h1>
-          <p>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout.
-          </p>
+        </div>
+        <div>
+          <div className={style.desc}>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
+          </div>
         </div>
         <div className={style.buttons}>
           <p>Product Commission: 12%</p>
