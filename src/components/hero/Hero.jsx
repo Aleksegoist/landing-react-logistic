dd026f7b740d32ffadb2cbd2fad174a7578fba0f
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-14 12:06:39 +0300

    add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,12 +7,14 @@ const Hero = () => {
       <div className='container'>
         <div className={style.hero_content_box}>
           <div className={style.hero_content}>
-            <h2>TRENDING PRODUCT</h2>
-            <h1>Business Network for Logistics</h1>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </p>
+            <div className={style.hero_title}>
+              <h2>TRENDING PRODUCT</h2>
+              <h1>Business Network for Logistics</h1>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </p>
+            </div>
             <div className={style.hero_btn}>
               <button>Product Commission: 12%</button>
               <button>Project Size Range: 500K USD</button>
