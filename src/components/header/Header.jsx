e5784c38115a5526592cc24d0c0627354b7035a9
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-14 18:10:20 +0300

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,6 +13,20 @@ const Header = () => {
             <button>Sign Up</button>
           </div>
         </div>
+        <div className={style.hero_content}>
+          <div className={style.hero_title}>
+            <h2>TRENDING PRODUCT</h2>
+            <h1>Business Network for Logistics</h1>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
+            <div className={style.hero_btn}>
+              <button>Product Commission: 12%</button>
+              <button>Project Size Range: 500K USD</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
